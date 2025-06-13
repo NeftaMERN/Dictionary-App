cd4cfb2330aaa265d125664ca01835036bf3e4a4
Neftalem <dneftalem@gmail.com>
2025-06-13 23:25:22 +0300

    add js codes add doc ids & add url and fuch

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+const btn = document.getElementById("serch-btn");
+
+btn.addEventListener("click", () => {
+
+    const ineWord = document.getElementById("ine_Word").Value;
+
+    fuch(`${url}${ineWord}`)
+    .then((response) => response.json)
+    .then((data => {
+
+        console.log(data);
+        result.innerHTML = `<div class="word">
+
+                <h3>Sampl</h3>
+
+                <button class="sound">
+
+                    <i class="fa-solid fa-volume-high"></i>
+
+                </button>
+
+            </div>
+
+            <div class="details">
+
+                <p>pos</p>
+                <p>/Sampl/</p>
+
+            </div>
+
+            <p class="word-mining">
+
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem, deleniti. Minus accusantium totam eius ut.
+
+            </p>
+
+            <p class="word-example">
+
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga labore voluptas itaque voluptate aut! Odit.
+
+            </p>`
+
+    }))
+
+})
